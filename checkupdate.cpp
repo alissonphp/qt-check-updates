5ce5192dc9766b42c078a1e361f2c76d46529501
Alisson <alisson.php@gmail.com>
2021-06-01 15:46:44 -0300

    ref(check): Change declaration methods

--- checkupdate.cpp
+++ checkupdate.cpp
@@ -7,29 +7,56 @@
 
 namespace  {
 static const char UpdatesEndpoint[] = "http://127.0.0.1:8080/updates.json";
-static const quint32 OneHour = 3600000;
+static const quint32 OneHour = 1000;
 }
 
-CheckUpdate::CheckUpdate() {
-    QTimer *timer = new QTimer(this);
-    timer->setTimerType(Qt::VeryCoarseTimer);
-    timer->start(2000);
-    connect(timer, &QTimer::timeout, this, &CheckUpdate::startCheckUpdates);
+class CheckUpdatePrivate {
+public:
+    QTimer *m_checkUpdatesTimer = nullptr;
+};
+
+CheckUpdate::CheckUpdate()
+    : d(new CheckUpdatePrivate)
+{
+    d->m_checkUpdatesTimer = new QTimer(this);
+    d->m_checkUpdatesTimer->setTimerType(Qt::VeryCoarseTimer);
+    d->m_checkUpdatesTimer->start(OneHour);
+    connect(
+                d->m_checkUpdatesTimer
+                , &QTimer::timeout
+                , this
+                , &CheckUpdate::startCheckUpdates
+                );
 }
 
-CheckUpdate::~CheckUpdate(){}
+CheckUpdate::~CheckUpdate(){
+    delete d;
+}
 
 void CheckUpdate::startCheckUpdates() {
-    getJson();
+    m_fileDownloader = new FileDownloader(QUrl(UpdatesEndpoint), this);
+    connect(m_fileDownloader, &FileDownloader::downloaded, this, &CheckUpdate::getJsonFromServer);
 }
 
-void CheckUpdate::getJson()
+void CheckUpdate::getJsonFromServer()
 {
-    FileDownloader *fd = new FileDownloader(QUrl(UpdatesEndpoint), this);
-    connect(fd, &FileDownloader::downloaded, this, &CheckUpdate::loadJson);
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(m_fileDownloader->downloadedData());
+    setParsedJson(jsonDocument);
 }
 
-QJsonObject CheckUpdate::loadJson(QByteArray data)
+void CheckUpdate::setParsedJson(QJsonDocument json)
 {
-  qDebug() << data;
+    m_json = json.object();
+    QString current {"2.0.9-4"};
+    QString checking = getParsedJson()["kernel"].toObject()["interfaces"].toObject()["version"].toString();
+    if( current != checking) {
+        qDebug() << "checking kernel :: interfaces :: version "<< current << QDateTime::currentDateTime().toString() << "NEW VERSION FOUND" << checking;
+    } else {
+        qDebug() << "checking kernel :: interfaces :: version "<< current << QDateTime::currentDateTime().toString() << "no updates found";
+    }
+}
+
+QJsonObject CheckUpdate::getParsedJson()
+{
+    return m_json;
 }
--- checkupdate.h
+++ checkupdate.h
@@ -4,21 +4,28 @@
 #include <QObject>
 #include <QJsonObject>
 
-class CheckUpdate : public QObject
+#include "filedownloader.h"
+
+class CheckUpdatePrivate;
+
+class CheckUpdate final : public QObject
 {
     Q_OBJECT
 public:
     explicit CheckUpdate();
     virtual ~CheckUpdate();
     void startCheckUpdates();
+    void setParsedJson(QJsonDocument json);
+    QJsonObject getParsedJson();
 
 signals:
     void newUpdatesAvailable(bool available);
 
 private:
-    void getJson();
-    QJsonObject loadJson(QByteArray data);
-
+    void getJsonFromServer();
+    CheckUpdatePrivate *d;
+    FileDownloader *m_fileDownloader;
+    QJsonObject m_json;
 };
 
 #endif // CHECKUPDATE_H
